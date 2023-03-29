f1a67f86a58e27dad14a9ec26f7707ae78e1fccc
menciatarinupv <menciatarinupv@gmail.com>
2023-03-29 10:27:47 +0200

    primer commit

---
+++ app/src/main/java/com/example/p_4/LogicaNegocio.java
@@ -0,0 +1,116 @@
+
+package com.example.p_4;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Optional;
+
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+public class LogicaNegocio {
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public interface Respuesta {
+        void callback ( Bundle resultados );
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    private static Optional<String> url_servidor = Optional.empty();
+    private static String servidor_por_defecto = "https://jsonplaceholder.typicode.com";
+
+    /*
+        Servidores REST de prueba
+
+        + https://jsonplaceholder.typicode.com
+        + https://httpbin.org
+
+https://jsonplaceholder.typicode.com
+
+/posts	100 posts
+/comments	500 comments
+/albums	100 albums
+/photos	5000 photos
+/todos	200 todos
+/users	10 users
+
+Routes:
+
+All HTTP methods are supported. You can use http or https for your requests.
+
+GET	/posts
+GET	/posts/1
+GET	/posts/1/comments
+GET	/comments?postId=1
+POST	/posts
+DELETE	/posts/1
+
+
+https://httpbin.org
+
+allows you to echo back your request, to see that it does what you expect.
+
+List of services (It echoes the data used in your request for any of these types)
+
+https://httpbin.org/anything Returns most of the below.
+https://httpbin.org/ip Returns Origin IP.
+https://httpbin.org/headers Returns header dict.
+https://httpbin.org/get Returns GET data.
+https://httpbin.org/post Returns POST data.
+https://httpbin.org/delete Returns DELETE data
+     */
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public static void ponerUrlServidor( String url ) {
+        Log.d( "primeraApp", "LogicaNegocio.ponerUrlServidor(): empieza");
+        url_servidor = Optional.of( url );
+        Log.d( "primeraApp", "LogicaNegocio.ponerUrlServidor(): termina");
+    } // ()
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public static void pedirAlgoAlServidorRest( String parametroQueNoUso, /*porque es una prueba*/
+                                               Respuesta responder
+    ) {
+        Log.d( "primeraApp", "LogicaNegocio.pedirAlgoAlServidorRest(): empieza");
+
+        PeticionarioREST elPeticionario = new PeticionarioREST();
+
+        elPeticionario.hacerPeticionREST(
+                "GET",
+                url_servidor.orElse(servidor_por_defecto) + "/users",
+                null,
+                new PeticionarioREST.RespuestaREST() { /* se puede abreviar: (codigo, cuerpo) -> { .... } */
+                    @Override
+                    public void callback(int codigo, String cuerpo) {
+                      Log.d( "primeraApp", "LogicaNegocio.pedirAlgoAlServidorRest().callback: recibo: " + codigo );
+
+                      /* aquí deberíamos analizar la respuesta REST+HTTP. Si es correcta, parsear el resultado
+                         que estará en JSON (pero como un único texto) para sacar las partes que nos interesen.
+                         
+                         Luego, ponemos las respuestas ya pasadas al tipo correcto en bundle (almacén clave-valor)
+                         y llamamos al callback que está esperando en la parte de UX
+                         */
+                      Bundle res = new Bundle();
+                      res.putInt( "codigo", codigo );
+                      res.putString( "resultadoSinParsear", cuerpo );
+                      responder.callback( res );
+
+                      Log.d( "primeraApp", "LogicaNegocio.pedirAlgoAlServidorRest().callback: recibo: " + cuerpo );
+
+                    }
+                } );
+
+
+        Log.d( "primeraApp", "LogicaNegocio.pedirAlgoAlServidorRest(): termina");
+    } // ()
+
+} // class
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
---
+++ app/src/main/java/com/example/p_4/MainActivity.java
@@ -0,0 +1,54 @@
+
+package com.example.p_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.p_4.LogicaNegocio;
+import com.example.p_4.PeticionarioREST;
+
+public class MainActivity extends AppCompatActivity {
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // <-- + o - sería MAIN
+        Log.d( "primeraApp", "MainActivity.onCreate(): empieza"); // <-- System.out.println
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prueba();
+
+        Log.d( "primeraApp", "MainActivity.onCreate(): acaba");
+    } // onCreate()
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public void prueba() {
+        Log.d( "primeraApp", "MainActivity.prueba(): empieza");
+
+        Log.d( "primeraApp", "MainActivity.prueba(): acaba");
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------
+    public void boton_pedir_pulsado( View quien ) { // <--- callback del boton
+        Log.d( "primeraApp", "MainActivity.boton_pedir_pulsado(): empieza");
+
+        LogicaNegocio.pedirAlgoAlServidorRest( "hola",
+                ( resultados ) -> {
+
+                    Log.d( "primeraApp", "MainActivity.boton_ok_pulsado(): resultados: "
+                            + resultados.getString("resultadoSinParsear" ) );
+
+                });
+
+
+        // dimePrecio()
+        Log.d( "primeraApp", "MainActivity.boton_pedir_pulsado(): acaba");
+    }
+}
